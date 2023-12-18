1bc4a4248caad6349d36f65751e90306116ce229
Avtar Singh <asm80717@gmail.com>
2023-12-18 19:23:34 +0530

    form js

--- js/form.js
+++ js/form.js
@@ -1,19 +1,29 @@
 function validateForm(formId) {
-        var form = document.getElementById(formId);
-        var inputs = form.querySelectorAll('input, select, textarea');
-        var isValid = true;
-    
-        inputs.forEach(function(input) {
-            if (input.hasAttribute('required') && input.value.trim() === '') {
-                isValid = false;
-            }
-            // Add more specific validations as needed for different fields
-        });
-    
-        if (!isValid) {
-            alert("Please fill in all required fields.");
-            return false;
-        }
-    
-        return true;
-    } 
+    var form = document.getElementById(formId);
+    var isValid = true;
+  
+    // Resetting previously highlighted fields
+    var inputs = form.querySelectorAll('input, select');
+    inputs.forEach(function(input) {
+      input.classList.remove('is-invalid');
+    });
+  
+    // Validating each field
+    inputs.forEach(function(input) {
+      if (input.hasAttribute('required') && input.value.trim() === '') {
+        input.classList.add('is-invalid');
+        isValid = false;
+      }
+    });
+  
+    if (!isValid) {
+      // If the form is invalid, prevent submission
+      return false;
+    }
+  
+    // If the form is valid, you can proceed with submission
+    // Example: You can submit the form using AJAX or let it submit naturally
+    // form.submit();
+  
+    return true;
+  }
