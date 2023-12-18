a9f5a63054405bf288ea908135563370ba82df3b
Avtar Singh <asm80717@gmail.com>
2023-12-18 19:28:10 +0530

    form ok

--- js/form.js
+++ js/form.js
@@ -1,29 +1,26 @@
 function validateForm(formId) {
-    var form = document.getElementById(formId);
-    var isValid = true;
-  
-    // Resetting previously highlighted fields
-    var inputs = form.querySelectorAll('input, select');
-    inputs.forEach(function(input) {
-      input.classList.remove('is-invalid');
-    });
-  
-    // Validating each field
-    inputs.forEach(function(input) {
-      if (input.hasAttribute('required') && input.value.trim() === '') {
-        input.classList.add('is-invalid');
-        isValid = false;
-      }
-    });
-  
-    if (!isValid) {
-      // If the form is invalid, prevent submission
-      return false;
+  var form = document.getElementById(formId);
+  var isValid = true;
+
+  // Resetting previously highlighted fields
+  var inputs = form.querySelectorAll('input, select');
+  inputs.forEach(function(input) {
+    input.classList.remove('is-invalid');
+  });
+
+  // Validating each field
+  inputs.forEach(function(input) {
+    if (input.hasAttribute('required') && input.value.trim() === '') {
+      input.classList.add('is-invalid');
+      isValid = false;
     }
-  
-    // If the form is valid, you can proceed with submission
-    // Example: You can submit the form using AJAX or let it submit naturally
-    // form.submit();
-  
-    return true;
+  });
+
+  if (!isValid) {
+    // If the form is invalid, prevent submission
+    event.preventDefault(); // Prevents form submission
+    return false;
   }
+
+  return true;
+}
