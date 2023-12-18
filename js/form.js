378b20560dea864dc24680d70a8f83973479a6af
Avtar Singh <asm80717@gmail.com>
2023-12-18 19:11:01 +0530

    form js update

--- js/form.js
+++ js/form.js
@@ -1,41 +1,19 @@
-    // Get the form element
-    const form = document.querySelector('.form-validate');
-
-    // Function to validate the form
-    function validateForm(event) {
-        // Prevent the form from submitting by default
-        event.preventDefault();
-
-        // Get the form fields
-        const name = form.querySelector('[name="yourname"]');
-        const email = form.querySelector('[name="email"]');
-        const mobile = form.querySelector('[name="Mobile_no"]');
-        const service = form.querySelector('[name="service_request"]');
-
-        // Check if fields are empty
-        if (name.value.trim() === '') {
-            alert('Please enter your name');
-            return;
+function validateForm(formId) {
+        var form = document.getElementById(formId);
+        var inputs = form.querySelectorAll('input, select, textarea');
+        var isValid = true;
+    
+        inputs.forEach(function(input) {
+            if (input.hasAttribute('required') && input.value.trim() === '') {
+                isValid = false;
+            }
+            // Add more specific validations as needed for different fields
+        });
+    
+        if (!isValid) {
+            alert("Please fill in all required fields.");
+            return false;
         }
-
-        if (email.value.trim() === '') {
-            alert('Please enter your email');
-            return;
-        }
-
-        if (mobile.value.trim() === '') {
-            alert('Please enter your mobile number');
-            return;
-        }
-
-        if (service.value === 'Select A Service') {
-            alert('Please select a service');
-            return;
-        }
-
-        // If all validations pass, submit the form
-        form.submit();
-    }
-
-    // Add event listener for form submission
-    form.addEventListener('submit', validateForm);
+    
+        return true;
+    } 
